db8afa4cb352779e41fe6c193eaf53ed303ac033
louayboughattas <74695114+louayboughattas@users.noreply.github.com>
2021-03-13 00:28:23 +0100

    epsi
    epsi

---
+++ scripts/form-5e27.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'epsi@.com',
+    'to' => ''
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'address' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Address',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Address\' is required.'
+    )
+    ),
+    'phone' => array(
+    'order' => 4,
+    'type' => 'tel',
+    'label' => 'Phone',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Phone\' is required.'
+    )
+    ),
+    'agree' => array(
+    'order' => 5,
+    'type' => 'checkbox',
+    'label' => 'I accept the Terms of Service',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'I accept the Terms of Service\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor();
+    $processor->process($form);
+
+    ?>
